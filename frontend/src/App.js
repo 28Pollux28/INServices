7573911b31b26b9d0b96e29b037540f53a46b1e7
Sow Mody <modysory100@gmail.com>
2023-06-06 16:16:19 +0200

    NAV bar conneted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import Button from './Button';
 import RoutesTree from "./RoutesTree";
 import ResponsiveAppBar from "./component/Header/Header";
-import NavBar from "./component/Header/Header";
+import {NavBarConnected,NavBarNotConnected} from "./component/Header/Header";
 import Footer from './component/Footer/Footer';
-
 function App() {
     return (
         <div className="App">
-            <NavBar/>
+            <NavBarConnected/>
             <RoutesTree/>
             <Footer/>
         </div>
