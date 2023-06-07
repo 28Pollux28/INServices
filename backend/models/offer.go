2f93d249e2c84259e9fd4f024244a9451931b6df
28Pollux28 <lemaire_valentin28@yahoo.fr>
2023-06-08 00:01:27 +0200

    Edit Offer model

--- backend/models/offer.go
+++ backend/models/offer.go
@@ -3,9 +3,13 @@ package models
 // Offer model
 type Offer struct {
 	Base
-	UserID           uint   `json:"user_id" gorm:"not null"`
-	OfferName        string `json:"offer_name" gorm:"not null"`
-	OfferDescription string `json:"offer_description" gorm:"not null"`
-	OfferPrice       uint   `json:"offer_price" gorm:"not null"`
-	OfferImage       string `json:"offer_image" gorm:""`
+	UserID      uint   `json:"user_id"`
+	Name        string `json:"title"`
+	Description string `json:"description"`
+	Price       uint   `json:"price"`
+	Image       string `json:"image"`
+	Visible     bool   `json:"visible" gorm:"default:true"`
+	Status      string `json:"status" gorm:"default:'available';"` // available, accepted, completed
+
+	AcceptedUserID uint `json:"accepted_user_id" gorm:"null"`
 }
