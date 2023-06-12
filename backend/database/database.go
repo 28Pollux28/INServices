e847795c286cff037f1fa5c03f0aca1cdd02f9f1
28Pollux28 <lemaire_valentin28@yahoo.fr>
2023-06-12 10:26:43 +0200

    Added SQLite DB and improve READMEs

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,22 +1,41 @@
 package database
 
 import (
+	"github.com/glebarez/sqlite"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"inservices/models"
 )
 
-// Connect with postgres database
-func Connect(dbUrl string) (*gorm.DB, error) {
+// ConnectPostgres with postgres database
+func ConnectPostgres(dbUrl string) (*gorm.DB, error) {
 	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
-	err = db.AutoMigrate(
+	err = migrateDB(db)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func migrateDB(db *gorm.DB) error {
+	err := db.AutoMigrate(
 		&models.User{},
 		&models.Offer{},
 		&models.RefreshToken{},
 	)
+	return err
+}
+
+// ConnectSqlite with sqlite database
+func ConnectSqlite(dbName string) (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open(dbName), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	err = migrateDB(db)
 	if err != nil {
 		return nil, err
 	}
