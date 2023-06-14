61af4c53799e5c6c08a15dad3ed4484390d65ffe
c-lee17 <chanbin.lee@insa-lyon.fr>
2023-06-15 00:23:41 +0200

    Page Mes Demandes

---
+++ frontend/src/view/MesDemandes/MesDemandes.js
@@ -0,0 +1,14 @@
+import Demande from "../../component/Header/Demande";
+
+const MesDemandes = () => {
+    return(
+        <>
+            <h3>Mes Demandes</h3>
+            
+            <Demande />
+        </>
+    
+    )
+}
+
+export default MesDemandes
