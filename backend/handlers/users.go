e11f845bbaa2eaffccdd61ae8d337c010fea42fd
28Pollux28 <lemaire_valentin28@yahoo.fr>
2023-06-15 15:46:14 +0200

    Limit name length and added edit endpoint Fix #12 Fix #26

--- backend/handlers/users.go
+++ backend/handlers/users.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"github.com/asaskevich/govalidator"
 	"github.com/disintegration/imaging"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"inservices/models"
 	"inservices/utils"
@@ -36,7 +38,7 @@ func UploadUserAvatar(c *fiber.Ctx) error {
 	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Internal server error1",
+			"error": "Internal server error",
 		})
 	}
 	id := claims["id"].(float64)
@@ -61,14 +63,14 @@ func UploadUserAvatar(c *fiber.Ctx) error {
 	err = imaging.Save(img50, "uploads/users/50/"+fName+".png")
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Internal server error6",
+			"error": "Internal server error",
 			"err":   err.Error(),
 		})
 	}
 	err = imaging.Save(img200, "uploads/users/200/"+fName+".png")
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Internal server error7",
+			"error": "Internal server error",
 		})
 	}
 
@@ -77,7 +79,7 @@ func UploadUserAvatar(c *fiber.Ctx) error {
 	res = db.Save(&user)
 	if res.Error != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Internal server error8",
+			"error": "Internal server error",
 		})
 	}
 	if oldAvatar != "" && oldAvatar != "default_avatar.png" {
@@ -93,6 +95,134 @@ func UploadUserAvatar(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
 		"avatar": fName + ".png",
 	})
+}
+
+func EditUser(c *fiber.Ctx) error {
+	token := c.Locals("user").(*jwt.Token)
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Internal server error",
+		})
+	}
+	id := claims["id"].(float64)
+	db := c.Locals("db").(*gorm.DB)
+	var user models.User
+	res := db.First(&user, id)
+	if res.Error != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "User not found",
+		})
+	}
+	var body struct {
+		Name                 string `json:"name" form:"name" name:"name"`
+		Surname              string `json:"surname" form:"surname" name:"surname"`
+		Username             string `json:"username" form:"username" name:"username"`
+		OldPassword          string `json:"old_password" form:"old_password" validate:"required,min=6,max=32" name:"old_password"`
+		Password             string `json:"password" form:"password" name:"password"`
+		PasswordConfirmation string `json:"password_confirmation" form:"password_confirmation" name:"password_confirmation"`
+		Email                string `json:"email" form:"email" name:"email"`
+		Phone                string `json:"phone" form:"phone" name:"phone"`
+	}
+	err := c.BodyParser(&body)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid body",
+		})
+	}
+	if body.Name != "" {
+		// Check length of name
+		if len(body.Name) > 32 {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Name is too long",
+			})
+		}
+		user.Name = body.Name
+	}
+	if body.Surname != "" {
+		// Check length of surname
+		if len(body.Surname) > 32 {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Surname is too long",
+			})
+		}
+		user.Surname = body.Surname
+	}
+	if body.Username != "" {
+		// Check length of username
+		if len(body.Username) > 32 {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Username is too long",
+			})
+		}
+		// Check if username is already taken
+		var tempUser models.User
+		res := db.Where("username = ?", body.Username).First(&tempUser)
+		if res.Error == nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Username is already taken",
+			})
+		}
+		user.Username = body.Username
+	}
+	if body.Email != "" {
+		// Check if email is valid
+		if !govalidator.IsEmail(body.Email) {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Email is not valid",
+			})
+		}
+		// Check if email is already taken
+		var tempUser models.User
+		res := db.Where("email = ?", body.Email).First(&tempUser)
+		if res.Error == nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Email is already taken",
+			})
+		}
+		user.Email = body.Email
+	}
+	if body.Phone != "" {
+		user.Phone = body.Phone
+	}
+	if body.Password != "" {
+		// Check length of password
+		if len(body.Password) < 8 || len(body.Password) > 32 {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Password must be between 8 and 32 characters",
+			})
+		}
+		// Check password strength
+		if ok1, ok2, ok3, ok4 := utils.CheckPasswordStrength(body.Password); ok1 && ok2 && ok3 && ok4 {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Password is too weak",
+			})
+		}
+		if body.Password != body.PasswordConfirmation {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Passwords do not match",
+			})
+		}
+		if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.OldPassword)) != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Incorrect password",
+			})
+		}
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(body.Password), 12)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Internal server error",
+			})
+		}
+		user.Password = string(hashedPassword)
+	}
+	res = db.Save(&user)
+	if res.Error != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Internal server error",
+		})
+	}
+	return c.JSON(user.Public())
 }
 
 func GetPrivUser(c *fiber.Ctx) error {
